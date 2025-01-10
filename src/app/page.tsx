da5383246ba5b824401182421134f297695ec71f
PupunhaDev <pupunha.dev@gmail.com>
2025-01-10 08:39:18 -0300

    instalação de daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import logo from "@/public/NextDS Logo 1.svg"
+
 export default function Home() {
   return (
-    <main>
-      <div>Primeiro commit da NextDS!</div>
+    <main className="flex size-full min-h-screen justify-center items-center">
+      <div className="flex flex-col items-center">
+        <Image src={logo} width={100} height={100} alt=""></Image>
+        <p className="text-white">Em breve o novo site da NextDS saindo...</p>
+        <button className="btn btn-accent"><span className="text-white">Entrar em contato</span></button>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
 
 export default {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {},
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 } satisfies Config;
